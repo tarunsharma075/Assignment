29fd931b7079be20880259766bbce4a423257816
Tarun <tarunkumarsharma38@gmail.com>
2025-04-23 18:11:24 +0530

    added the assignment of linked list

---
+++ Linked List Loop - Linked List.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* Next;
+};
+
+void CheckLoop(Node* head) {
+    Node* temp = head;
+    unordered_map<Node*, int> mpp;
+
+    while (temp != nullptr) {
+        if (mpp.find(temp) != mpp.end()) {
+            cout << "There is a loop" << endl;
+            return;
+        } else {
+            mpp[temp] = 1;
+        }
+        temp = temp->Next;
+    }
+
+    cout << "There is no loop" << endl;
+}
+
+int main() {
+    Node* head;
+    
+// the code where the loop exists 
+// please uncomment  the loop you want to check
+
+//    Node* first = new Node();
+//    Node* second = new Node();
+//    Node* third = new Node();
+//    Node* fourth = new Node();
+//    Node* fifth = new Node();
+//
+//    first->data = 10;
+//    first->Next = second;
+//
+//    second->data = 20;
+//    second->Next = third;
+//
+//    third->data = 30;
+//    third->Next = fourth;
+//
+//    fourth->data = 40;
+//    fourth->Next = fifth;
+//
+//    fifth->data = 50;
+//    fifth->Next = third; 
+
+
+//the code where loop dosent exist
+
+
+//    Node* first = new Node();
+//    Node* second = new Node();
+//    Node* third = new Node();
+//    Node* fourth = new Node();
+//    Node* fifth = new Node();
+//
+//    first->data = 10;
+//    first->Next = second;
+//
+//    second->data = 20;
+//    second->Next = third;
+//
+//    third->data = 30;
+//    third->Next = fourth;
+//
+//    fourth->data = 40;
+//    fourth->Next = fifth;
+//
+//    fifth->data = 50;
+//    fifth->Next = nullptr; 
+
+    head = first;
+
+    CheckLoop(head);
+}
+
